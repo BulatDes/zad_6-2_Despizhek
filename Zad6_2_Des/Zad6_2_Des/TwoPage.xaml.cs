4f80ab71436dd316e69604c56291b1e04940fb6c
BulatDes <123715505+BulatDes@users.noreply.github.com>
2023-06-26 15:50:03 +0500

    Add files via upload

---
+++ Zad6_2_Des/Zad6_2_Des/TwoPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Zad6_2_Des
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TwoPage :ContentPage
+    {
+        public TwoPage (Hotel hotel)
+        {
+            InitializeComponent();
+        }
+    }
+}
